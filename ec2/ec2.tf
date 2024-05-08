ae74b9687f17cc97b1155c8fb6c2b0eb31a10fc0
Navaneethraot <youremail@gmail.com>
2024-05-08 19:48:42 +0530

    terraform

---
+++ ec2/ec2.tf
@@ -0,0 +1,39 @@
+resource "aws_instance" "test" {
+
+    ami= "ami-090252cbe067a9e58"
+    instance_type= "t3.micro"
+    vpc_security_group_ids =[aws_security_group.allow_ssh.id]
+
+
+    tags = {
+     name= "db"
+    }
+
+  }
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allowing ssh access"
+
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }  
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol =  -1
+    cidr_blocks = ["0.0.0.0/0"]
+
+  }
+
+  tags ={
+    name= "allow ssh"
+    createdby="Navaneethrao"
+  }
+
+  
+
+} 
